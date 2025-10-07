516fc075efd9d6d75f683dc24a449a74c6164671
UsairumKhan <usairumkhan999@gmail.com>
2025-10-07 20:35:51 +0500

    feat: add -l long listing support (v1.1.0)

--- src/ls.c
+++ src/ls.c
@@ -1,63 +1,101 @@
-/*
-* Programming Assignment 02: lsv1.0.0
-* This is the source file of version 1.0.0
-* Read the write-up of the assignment to add the features to this base version
-* Usage:
-*       $ lsv1.0.0 
-*       % lsv1.0.0  /home
-*       $ lsv1.0.0  /home/kali/   /etc/
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
-#include <string.h>
-#include <errno.h>
 #include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <string.h>
 
-extern int errno;
+/* Function prototypes */
+void do_ls(const char *dirname, int long_format);
+void show_details(const char *path, const char *filename);
 
-void do_ls(const char *dir);
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
-        do_ls(".");
-    }
+    int long_format = 0;
+
+    /* check if -l option is given */
+    if (argc > 1 && strcmp(argv[1], "-l") == 0)
+        long_format = 1;
+
+    /* if only ./bin/ls -l */
+    if (argc == 1 || (argc == 2 && long_format))
+        do_ls(".", long_format);
     else
     {
-        for (int i = 1; i < argc; i++)
+        for (int i = (long_format ? 2 : 1); i < argc; i++)
         {
-            printf("Directory listing of %s : \n", argv[i]);
-            do_ls(argv[i]);
-	    puts("");
+            printf("Directory listing of %s:\n", argv[i]);
+            do_ls(argv[i], long_format);
+            puts("");
         }
     }
     return 0;
 }
 
-void do_ls(const char *dir)
+/* List directory contents */
+void do_ls(const char *dirname, int long_format)
 {
-    struct dirent *entry;
-    DIR *dp = opendir(dir);
-    if (dp == NULL)
+    DIR *dp = opendir(dirname);
+    if (!dp)
     {
-        fprintf(stderr, "Cannot open directory : %s\n", dir);
+        perror("opendir");
         return;
     }
-    errno = 0;
+
+    struct dirent *entry;
     while ((entry = readdir(dp)) != NULL)
     {
-        if (entry->d_name[0] == '.')
+        if (entry->d_name[0] == '.')  /* skip hidden */
             continue;
-        printf("%s\n", entry->d_name);
-    }
 
-    if (errno != 0)
-    {
-        perror("readdir failed");
+        if (long_format)
+            show_details(dirname, entry->d_name);
+        else
+            printf("%s\n", entry->d_name);
     }
 
     closedir(dp);
+}
+
+/* Print file details like ls -l */
+void show_details(const char *path, const char *filename)
+{
+    char fullpath[1024];
+    struct stat st;
+    snprintf(fullpath, sizeof(fullpath), "%s/%s", path, filename);
+
+    if (lstat(fullpath, &st) == -1)
+    {
+        perror("lstat");
+        return;
+    }
+
+    /* Permissions */
+    printf((S_ISDIR(st.st_mode)) ? "d" : "-");
+    printf((st.st_mode & S_IRUSR) ? "r" : "-");
+    printf((st.st_mode & S_IWUSR) ? "w" : "-");
+    printf((st.st_mode & S_IXUSR) ? "x" : "-");
+    printf((st.st_mode & S_IRGRP) ? "r" : "-");
+    printf((st.st_mode & S_IWGRP) ? "w" : "-");
+    printf((st.st_mode & S_IXGRP) ? "x" : "-");
+    printf((st.st_mode & S_IROTH) ? "r" : "-");
+    printf((st.st_mode & S_IWOTH) ? "w" : "-");
+    printf((st.st_mode & S_IXOTH) ? "x" : "-");
+
+    /* Owner, group, size, time, name */
+    struct passwd *pw = getpwuid(st.st_uid);
+    struct group *gr = getgrgid(st.st_gid);
+    char *time_str = ctime(&st.st_mtime);
+    time_str[strlen(time_str) - 1] = '\0'; /* remove newline */
+
+    printf(" %ld %s %s %ld %s %s\n",
+           (long)st.st_nlink,
+           pw ? pw->pw_name : "?",
+           gr ? gr->gr_name : "?",
+           (long)st.st_size,
+           time_str,
+           filename);
 }
